024a9a643fa57beeac4fa843bc2f33a8e5bc6930
jamuna23 <jamunamaharjan19@gmail.com>
2023-02-09 13:37:08 -0700

    Update akamai.tf

--- akamai.tf
+++ akamai.tf
@@ -10,12 +10,7 @@ terraform {
 # Configure the Akamai Provider
 provider "akamai" {
 
-config{
-client_secret = "J/AZO9hXUlUOU4d5pM9Lc+lS8sgMcaB0s9o7eYgk7+o=" 
-host = "akab-75dlnrw66zwvyvk5-ulxoodt7x6hdibda.luna.akamaiapis.net" 
-access_token = "akab-3avxndlqgz7poyhm-3nfaunyxt4b5ex4e" 
-client_token = "akab-wuuor5zdra2hjm5s-iaqo6p3tc4phwq6s" 
-}
+
 
 
   }
